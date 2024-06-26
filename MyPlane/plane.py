a98c9873d85c84246972b631f5511d4468c9e99d
ARCHIMEDESCOMPU\Archimedes <junhao_jia@outlook.com>
2024-06-26 14:49:05 +0800

    添加敌机炸毁动画

--- MyPlane/plane.py
+++ MyPlane/plane.py
@@ -10,13 +10,16 @@ WIDTH,HEIGHT = 800,600
 CREATE_ENEMY = pygame.USEREVENT
 #初始化
 pygame.init()
+pygame.time.set_timer(CREATE_ENEMY,200)
 #创建游戏窗口
 screen = pygame.display.set_mode((800,600))
 #游戏循环
 pygame.display.set_caption("飞机大战")
 #设置系统时钟
 clock = pygame.time.Clock()
-pygame.time.set_timer(CREATE_ENEMY,1000)
+
+#添加背景音乐
+
 
 #初始化英雄
 class Hero(pygame.sprite.Sprite):
@@ -24,6 +27,9 @@ class Hero(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("./images/me1.png")
         self.rect = self.image.get_rect()
+        self.rect.width *= 0.7
+        self.rect.height *= 0.7
+        self.image = pygame.transform.scale(self.image,(self.rect.width,self.rect.height))
         self.rect.x = 80
         self.rect.y = 60
         self.speed = speed
@@ -63,6 +69,8 @@ class Bullet(pygame.sprite.Sprite):
 
     def update(self, *args):
         self.rect.x += self.speed
+        if self.rect.x > WIDTH:
+            self.kill()
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self,speed):
@@ -74,7 +82,26 @@ class Enemy(pygame.sprite.Sprite):
         self.speed = speed
 
     def update(self, *args):
-       self.rect.x -= self.speed
+        self.rect.x -= self.speed
+        if self.rect.x <= 0:
+            self.kill()
+
+class Explode(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = [pygame.image.load("./images/enemy1_down"+str(i)+".png") for i in range(1,4)]
+        self.image_index = 0
+        self.image = self.images[self.image_index]
+        self.rect = self.image.get_rect()
+        self.ready_to_change = 0
+    def update(self, *args):
+        if self.image_index < 2:
+            self.ready_to_change += 1
+            if self.ready_to_change % 4 == 0:
+                self.image_index += 1
+                self.image = self.images[self.image_index]
+        else:
+            self.kill()
 
 class BackGround(pygame.sprite.Sprite):
     def __init__(self):
@@ -103,6 +130,7 @@ bg_group = pygame.sprite.Group()
 hero_group = pygame.sprite.Group()
 enemy_group = pygame.sprite.Group()
 bullet_group = pygame.sprite.Group()
+explode_group = pygame.sprite.Group()
 hero_group.add(hero)
 bg_group.add(bg,bg1)
 
@@ -121,18 +149,14 @@ while True:
     #游戏更新
     screen.fill(BLACK)
     #碰撞检测
-    pygame.sprite.groupcollide(enemy_group,bullet_group,True,True)
+    collision = pygame.sprite.groupcollide(enemy_group,bullet_group,True,True)
+    for enemy in collision.keys():
+        explode = Explode()
+        explode.rect = enemy.rect
+        explode_group.add(explode)
 
-    # hero_group.update()
-    # enemy_group.update()
-    # bullet_group.update()
-    # #屏幕更新
-    # hero_group.draw(screen)
-    # enemy_group.draw(screen)
-    # bullet_group.draw(screen)
-    # pygame.display.update()
     #屏幕更新
-    for group in [bg_group,hero_group, enemy_group, bullet_group]:
+    for group in [bg_group,hero_group, enemy_group, bullet_group,explode_group]:
         group.update()
         group.draw(screen)
     pygame.display.update()
